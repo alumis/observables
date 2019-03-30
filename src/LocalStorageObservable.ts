364cb4c46e41b50a6f18b93ca2dbacfff84e9df8
Emil Müller <emil@alumis.no>
2019-03-30 23:10:00 +0100

    no message

--- src/LocalStorageObservable.ts
+++ src/LocalStorageObservable.ts
@@ -113,27 +113,27 @@ let cache = new Map<string, LocalStorageObservable<any>>();
 
     let hidden, visibilityChange;
 
-    if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+    if (typeof window.document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
 
         hidden = "hidden";
         visibilityChange = "visibilitychange";
     }
 
-    else if (typeof document["msHidden"] !== "undefined") {
+    else if (typeof window.document["msHidden"] !== "undefined") {
 
         hidden = "msHidden";
         visibilityChange = "msvisibilitychange";
     }
 
-    else if (typeof document["webkitHidden"] !== "undefined") {
+    else if (typeof window.document["webkitHidden"] !== "undefined") {
 
         hidden = "webkitHidden";
         visibilityChange = "webkitvisibilitychange";
     }
 
-    document.addEventListener(visibilityChange, () => {
+    window.document.addEventListener(visibilityChange, () => {
 
-        if (!document[hidden])
+        if (!window.document[hidden])
             cache.forEach(v => { v.refresh(); });
 
     }, false);
