2c63d732b692f395d51a23d63ff89ef34348a01c
andrewsadduq <andrewnielsen89@gmail.com>
2019-05-08 17:37:26 +0200

    ...

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 export * from "./src/Observable";
 export * from "./src/LocalStorageObservable";
 export * from "./src/SessionStorageObservable";
+export * from './src/IObservablePromise';
+export * from './src/IObservablePromiseWithText';
+export * from './src/createObservablePromise';
+export * from './src/createObservablePromiseWithText';
