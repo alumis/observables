d1758c999b1db8ed1e2fa2d0dfe24143a23634a0
Emil Müller <emil@alumis.no>
2019-08-03 01:06:37 +0200

    no message

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 export * from "./src/Observable";
 export * from "./src/whenAsync";
+export * from "./src/ObservableSet";
+export * from "./src/ObservableList";
+export * from "./src/FilteredObservableSet";
+export * from "./src/SortedObservableSet";
+export * from "./src/MappedObservableList";
