b88db2e8b5334e42bf504c562b67e3d0278123a1
Emil Müller <emil@alumis.no>
2019-08-09 22:13:38 +0200

    bumped

--- src/whenAsync.ts
+++ src/whenAsync.ts
@@ -1,21 +1,58 @@
+import { CancellationToken } from "@alumis/utils/src/CancellationToken";
+import { OperationCancelledError } from "@alumis/utils/src/OperationCancelledError";
 import { co } from "./Observable";
 
-export function whenAsync(expression: () => boolean) {
-    return new Promise((resolve, reject) => {
-        let o = co(() => {
-            try {
-                return expression();
-            }
-            catch (e) {
-                o.dispose();
-                reject(e);
+export function whenAsync(expression: () => boolean, cancellationToken?: CancellationToken) {
+    if (cancellationToken) {
+        return new Promise((resolve, reject) => {
+            let listener: () => void, notRejected = true, o = co(() => {
+                try {
+                    return expression();
+                }
+                catch (e) {
+                    if (listener)
+                        cancellationToken.removeListener(listener);
+                    else notRejected = false;
+                    o.dispose();
+                    reject(e);
+                }
+            });
+            if (notRejected) {
+                listener = () => {
+                    o.dispose();
+                    reject(new OperationCancelledError());
+                };
+                cancellationToken.addListener(listener);
+                o.subscribeInvoke(n => {
+                    if (n) {
+                        cancellationToken.removeListener(listener);
+                        o.dispose();
+                        resolve(n);
+                    }
+                });
             }
         });
-        o.subscribeInvoke(n => {
-            if (n) {
-                o.dispose();
-                resolve(n);
+    }
+    else {
+        return new Promise((resolve, reject) => {
+            let notRejected = true, o = co(() => {
+                try {
+                    return expression();
+                }
+                catch (e) {
+                    notRejected = false;
+                    o.dispose();
+                    reject(e);
+                }
+            });
+            if (notRejected) {
+                o.subscribeInvoke(n => {
+                    if (n) {
+                        o.dispose();
+                        resolve(n);
+                    }
+                });
             }
         });
-    });
+    }
 }
